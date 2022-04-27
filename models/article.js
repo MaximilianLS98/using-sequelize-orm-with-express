ce7ebd91b6bc1d1080047207f97bd7523cbecae3
MaximilianLS98 <49490417+MaximilianLS98@users.noreply.github.com>
2022-04-27 23:30:28 +0200

    small test of sequelize and CRUD operations with SQLite

---
+++ models/article.js
@@ -0,0 +1,27 @@
+'use strict';
+const Sequelize = require('sequelize');
+const moment = require('moment');
+
+module.exports = (sequelize) => {
+  class Article extends Sequelize.Model {
+      publishedAt() {
+          const date = moment(this.createdAt).format('D MMMM, YYYY, h:mma');
+          return date;
+      }
+      shortDescription() {
+          const shortDesc = this.body.length > 200 ? this.body.substring(0, 200) + "…" : this.body;
+          return shortDesc;
+      }
+    //   shortDescription() {
+    //     const shortDesc = this.body.length > 200 ? this.body.substring(0, 200) + "..." : this.body;
+    //     return shortDesc;
+    //   }
+  }
+  Article.init({
+      title: Sequelize.STRING,
+      author: Sequelize.STRING,
+      body: Sequelize.TEXT
+  }, { sequelize });
+
+  return Article;
+};
